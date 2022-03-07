8c03b4de84bc6c24a67d59c0286a81a6f029613e
Fahed Jubair <fjubair@gmail.com>
2022-03-07 12:47:34 +0300

    add src & lib

---
+++ src/Solution.java
@@ -0,0 +1,16 @@
+
+public class Solution {
+
+    /***
+     * 
+     * @param n: the numebr of students
+     * @param d: the maximum allowed score difference for students within the same team 
+     * @param scores: an array containing student scores
+     * @return the minimal number of teams 
+     */
+    public static int numOfGroups(int n, int d, int[] scores){
+        // write your code here to return the smallest number of teams
+        return 0;
+    }
+
+}
